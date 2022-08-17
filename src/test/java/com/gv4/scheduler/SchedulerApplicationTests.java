16977f035225583748120be3be5e27ba17ef00aa
mikolajmialkowski <mikolaj.mialkowski@outlook.com>
2022-08-17 16:07:15 +0200

    Initial commit

---
+++ src/test/java/com/gv4/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gv4.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
